cc6a1179c8fa9b69956d382fea85efed6def9981
min-0 <dhe08133@naver.com>
2021-02-05 17:02:59 +0900

    Objects

---
+++ Practice/min0Info.js
@@ -0,0 +1,19 @@
+const min0Info = {
+    name : "Min0",
+    age : 21,
+    gender: "Female",
+    isBeauti : true,
+    fovMovies : ["frozen2", "Aladdin"],
+    fovFood: [
+        {
+            name: "curry",
+            fatty: false
+        },
+        {
+            name: "cheese sticks",
+            fatty: true
+        }
+    ]
+}
+
+console.log(min0Info)
