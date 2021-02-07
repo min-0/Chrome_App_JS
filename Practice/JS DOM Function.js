13ba48d49cc524152d3a88bbd05433fc373af914
min-0 <dhe08133@naver.com>
2021-02-07 22:42:39 +0900

    Function

---
+++ Practice/JS DOM Function.js
@@ -0,0 +1,7 @@
+
+const title = document.querySelector("#title"); //const title = document.getElementById("title");
+title.innerHTML = "Hi! From JS";
+title.style.color  = "blue";
+document.title = "I own you now";
+
+//DOM //Document Object Module
